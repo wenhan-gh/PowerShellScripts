4720936a2813d816d56df8cc70950c355da00c13
wenhan-gh <80583474+wenhan-gh@users.noreply.github.com>
2021-05-21 05:09:57 +0800

    Initial Commit of a script

---
+++ MovePhotos.ps1
@@ -0,0 +1,20 @@
+﻿$a = Get-ChildItem G:\PHOTO\TOBESORTED
+$dest = 'G:\PHOTO\From-Whatsapp'
+$a | ForEach-Object{ $file = $_.name;$full = $_.fullname
+switch -wildcard ($file) {
+ "IMG*" {Write-Host $file
+ [string]$date = ($file.split("-"))[1]
+ $date
+ $datetime = [datetime]::ParseExact($date,"yyyyMMdd",$null)
+ $datetime
+ $year = $datetime.Year
+ $month = $datetime.ToString("MM")
+ $Directory = $dest + "\" + $year + "-" + $month + "\"
+ $Directory
+ if (!(Test-Path $Directory)) {New-Item $Directory -type directory}}
+
+}#ENDSWITCH
+
+Move-Item $full $Directory
+
+}#ENDFOREACH
