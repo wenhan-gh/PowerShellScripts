f9ff72f60afe63c6c2edf1d0b747f20d475afd57
wenhan-gh <80583474+wenhan-gh@users.noreply.github.com>
2021-05-21 05:15:29 +0800

    Add files via upload

---
+++ ChungWenHan-wenhanchung@pm.me.ps1
@@ -0,0 +1,76 @@
+﻿#REQUIRES -Version 5.0
+<#
+.LINK
+    Copy of script also available on my github at:
+    https://github.com/wenhan-gh/PowerShellScripts
+    or a direct link at:
+    https://github.com/wenhan-gh/PowerShellScripts/raw/main/ChungWenHan-wenhanchung@pm.me.ps1
+#>
+
+function listChildFolder($folderPath) {
+    <#
+    .Description
+    Lists items from given folder path.
+    #>
+
+    try {
+        If ( (Get-ChildItem $folderPath -ErrorAction Stop | Measure-Object).count -eq 0 )
+            {
+                Write-Output "Folder is empty."
+            }
+        else
+            {
+                $folderContent = Get-ChildItem $folderPath -ErrorAction Stop | Sort-Object LastWriteTime
+                [array]$listofNames = $folderContent | ForEach-Object {$_.Name}
+                [string]$result = $null
+                $result = '"{0}"' -f ($listofNames -join '","')
+                return $result
+            }
+    }
+    catch [System.IO.DirectoryNotFoundException],[System.IO.FileNotFoundException],[System.Management.Automation.ItemNotFoundException] {
+        # Catch not found exceptions.
+        throw "ERROR: Cannot locate path: [ $folderPath ]."
+    }
+    catch [System.IO.IOException] {
+        # Catch if there are any IO errors.
+        throw "ERROR: IO error with path: [ $folderPath ]."
+    }
+}
+
+function compareTwoFiles([String]$firstFilePath, [String]$secondFilePath) {
+    <#
+    .Description
+    Compares the sizes of two files and returns which is largers.
+    #>
+
+    #Write-Host ("1st file size is: " + $1stFile.Length/1KB + " kilobytes")
+    #Write-Host ("2nd file size is: " + $2ndFile.Length/1KB + " kilobytes")
+
+    # Check files first.
+    try {
+        $1stFile = Get-ChildItem $firstFilePath -ErrorAction Stop
+    }
+    catch [System.IO.FileNotFoundException],[System.Management.Automation.ItemNotFoundException] {
+        throw "ERROR: First given file not found."
+    }
+    try {
+        $2ndFile = Get-ChildItem $secondFilePath -ErrorAction Stop   
+    }
+    catch [System.IO.FileNotFoundException],[System.Management.Automation.ItemNotFoundException] {
+        throw "ERROR: Second given file not found."
+    }
+
+    # Can now continue after checks.
+    if ($1stFile.Length -eq $2ndFile.Length) {
+        Write-Host ("Both files have the same size.")
+    }
+    elseif ($1stFile.Length -gt $2ndFile.Length) {
+        #Write-Host ("File [ " + $1stFile.Name + " ] is larger than file [ " + $2ndFile.Name + " ].")
+        Write-Host ("File [ " + $1stFile.Name + " ] is larger.")
+    }
+    elseif ($1stFile.Length -lt $2ndFile.Length) {
+        #Write-Host ("File [ " + $2ndFile.Name + " ] is larger than file [ " + $1stFile.Name + " ].")
+        Write-Host ("File [ " + $2ndFile.Name + " ] is larger.")
+    }
+}
+
